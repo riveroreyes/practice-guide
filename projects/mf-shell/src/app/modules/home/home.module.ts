971ca886d94a03329839e669362424c1adad2072
Carlos Rivero <riveroreyes@hotmail.com>
2023-02-04 18:34:00 +0100

    several practices

--- projects/mf-shell/src/app/modules/home/home.module.ts
+++ projects/mf-shell/src/app/modules/home/home.module.ts
@@ -5,13 +5,19 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { ProductComponent } from './product/product.component';
 import { ShoppingComponent } from './shopping/shopping.component';
+import { CompanyComponent } from './company/company.component';
+import { TaskComponent } from './task/task.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     ProductComponent,
-    ShoppingComponent
+    ShoppingComponent,
+    CompanyComponent,
+    TaskComponent,
+    ConfigurationComponent
   ],
   imports: [
     CommonModule,
