d0a607057fe4c27b4300df9306470ffbe43b6310
Carlos Rivero <riveroreyes@hotmail.com>
2023-02-05 12:19:57 +0100

    Script to deploy in github

--- projects/mf-shell/src/app/modules/home/home.component.spec.ts
+++ projects/mf-shell/src/app/modules/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +9,10 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports:[
+        ActivatedRoute
+      ]
     })
     .compileComponents();
 
